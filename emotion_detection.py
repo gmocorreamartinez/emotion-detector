4af822b79f0db517648e12f1ffe56d83a0bc9c17
Guillermo Correa M <guillermocorreamartinez@gmail.com>
2025-08-11 00:06:05 -0400

    Modificacion archivo emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,10 +1,10 @@
 import requests
 
-URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-HEADERS = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
 
 def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     
-    payload = {"raw_document": {"text": text_to_analyze}}
-    resp = requests.post(URL, headers=HEADERS, json=payload)
+    myobj = {"raw_document": {"text": text_to_analyze}}
+    response = requests.post(url, json=myobj, headers=headers)
     return resp.text 
